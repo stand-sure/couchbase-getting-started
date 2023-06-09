7a96ac18f850446c3a9158ccdb7b40dd79eed721
C. J. Anderson <chris@standsure.io>
2023-06-09 10:25:37 -0400

    poc graphql

---
+++ CouchGraphQl/Program.cs
@@ -0,0 +1,32 @@
+namespace CouchGraphQl;
+
+using JetBrains.Annotations;
+
+/// <summary>
+/// </summary>
+[UsedImplicitly]
+public class Program
+{
+    /// <summary>
+    /// </summary>
+    protected Program()
+    {
+    }
+
+    private static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        builder.Host.ConfigureHostBuilder();
+
+        builder.Host.ConfigureServices(ProgramConfiguration.ServiceConfiguration);
+
+        WebApplication app = builder.Build();
+
+        app.ConfigureApplicationBuilder();
+
+        app.ConfigureEndpointRouteBuilder();
+
+        app.Run();
+    }
+}
